2a17610c65b69ce10e120db1b1691e7b30dcec81
InvI <sanim46.sk@gmail.com>
2020-01-30 08:45:04 +0545

    aasd

---
+++ app/src/main/java/com/example/googlemapsapp/SanimActivity.java
@@ -0,0 +1,14 @@
+package com.example.googlemapsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SanimActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sanim);
+    }
+}
